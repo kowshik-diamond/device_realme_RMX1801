1ec4227d739ad78fd96c13514310838ffd0ac51a
baibhab <fakebai34@gmail.com>
2021-03-29 08:52:22 +0530

    RMX1801: don't build thermal qti for now
    add thermal@1.0 back

--- device.mk
+++ device.mk
@@ -423,7 +423,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
